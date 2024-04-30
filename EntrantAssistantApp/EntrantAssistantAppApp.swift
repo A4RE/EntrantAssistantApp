26d0d5bb47f831ca36546d4b7209d13d1437573b
Andrei <aakoval01@gmail.com>
2024-04-30 11:04:18 +0300

    Initial Commit

---
+++ EntrantAssistantApp/EntrantAssistantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntrantAssistantAppApp.swift
+//  EntrantAssistantApp
+//
+//  Created by Андрей Коваленко on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EntrantAssistantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
